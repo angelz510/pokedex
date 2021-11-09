8e5279c2a8ff6c1ed4e5ace1f4eec0be7d595f26
Angel R <angelzrod4@gmail.com>
2021-11-09 13:13:22 -0800

    refactored styling to use styled components and added feature where pokemon card color changed based on pokemon type

--- src/components/PokemonStats/PokemonStats.js
+++ src/components/PokemonStats/PokemonStats.js
@@ -1,26 +1,56 @@
 import React from "react";
+import styled from "styled-components";
 import "./PokemonStats.css";
 
+// const DetailsCard = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   width: 100%;
+//   text-transform: capitalize;
+//   font-size: 1.13rem;
+// `;
+
+const PokemonDetails = styled.div`
+  display: flex;
+`;
+
+const PokemonArt = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-right: 1rem;
+`;
+
+const PokeImg = styled.img`
+  height: 15rem;
+`;
+
+const PokeStats = styled.div`
+  margin-left: 1rem;
+`;
+
 const PokemonStats = ({ pokemon }) => {
+  const style = `details-card ${pokemon.types[0].type.name}`;
   return (
-    <div className="stats-container">
-      <p>#{pokemon.id}</p>
-      <div className="pokemon-details">
-        <div className="pokemon-art">
-          <img
+    <div className={style}>
+      <h3>#{pokemon.id}</h3>
+      <PokemonDetails>
+        <PokemonArt>
+          <PokeImg
             src={pokemon.sprites.other["official-artwork"].front_default}
             alt={pokemon.name}
           />
-          <p className="pokemon-name">{pokemon.name}</p>
-        </div>
-        <div className="pokemon-stats">
+          <h2>{pokemon.name}</h2>
+        </PokemonArt>
+        <PokeStats>
           <p>Type: {pokemon.types[0].type.name}</p>
           <p>hp: {pokemon.stats[0].base_stat}</p>
           <p>Attack: {pokemon.stats[1].base_stat}</p>
           <p>Defense: {pokemon.stats[2].base_stat}</p>
           <p>Speed: {pokemon.stats[5].base_stat}</p>
-        </div>
-      </div>
+        </PokeStats>
+      </PokemonDetails>
     </div>
   );
 };
